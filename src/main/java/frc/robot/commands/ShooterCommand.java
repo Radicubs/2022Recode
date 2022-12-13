cec26b16c4f5865b043eb1d3e98f3aef1e9ae4c8
Kavinraj23 <kavin.raj@outlook.com>
2022-12-12 19:33:33 -0600

    Worked on DriveBase

---
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -0,0 +1,43 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.Shooter;
+
+public class ShooterCommand extends CommandBase {
+
+    private final Shooter shooter;
+    private int count;
+
+    public ShooterCommand(Shooter shooter) {
+        this.shooter = shooter;
+        this.count = 0;
+        addRequirements(shooter);
+    }
+
+    @Override
+    public void initialize() {
+        shooter.set(0, 0);
+    }
+
+    @Override
+    public void execute() {
+        count++;
+        if(RobotContainer.joystick.getRawButton(0)) {
+            shooter.set(1, 1);
+        } else {
+            shooter.set(0, 0);
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooter.set(0, 0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return count >= 50;
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.ShooterCommand;
+
+public class Shooter extends SubsystemBase {
+    private final WPI_TalonFX topMotor;
+    private final WPI_TalonFX bottomMotor;
+
+    public Shooter() {
+        topMotor = new WPI_TalonFX(0);
+        bottomMotor = new WPI_TalonFX(0);
+        setDefaultCommand(new ShooterCommand(this));
+    }
+
+    public void set(double top, double bottom) {
+        topMotor.set(ControlMode.PercentOutput, top);
+        bottomMotor.set(ControlMode.PercentOutput, bottom);
+    }
+}
