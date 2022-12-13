57c69494601e81b762af47816b7ed1c7e3b9965c
SathvikY3533 <sathvik.y007@gmail.com>
2022-12-12 19:38:58 -0600

    Created Shooter Command+Subsystem w/Jacob

--- src/main/java/frc/robot/commands/shooterCommand.java
+++ src/main/java/frc/robot/commands/shooterCommand.java
@@ -3,7 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 
-public class shooterComand extends CommandBase {
+public class shooterCommand extends CommandBase {
 
     private final Shooter shooter;
     private int count;
